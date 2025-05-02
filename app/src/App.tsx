1a9e26a7e45cdf32cfd56d7669c48769eb1580f4
Lilach E <lilachelayza@gmail.com>
2025-05-02 11:08:52 +0300

    remove main title

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,6 @@ import Game from "./components/Game";
 function App() {
   return (
     <div className="App">
-      <h1>High-Tech Quest</h1>
       <div style={{ width: "1280px", height: "720px", margin: "0 auto" }}>
         <Game />
       </div>
