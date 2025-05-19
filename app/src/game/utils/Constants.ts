c152ad24ce4a839de83bf15aff1289fef6627d47
Lilach E <lilachelayza@gmail.com>
2025-05-19 17:49:52 +0300

    Remove ScoreDisplay.ts and score constants

--- app/src/game/utils/Constants.ts
+++ app/src/game/utils/Constants.ts
@@ -1,5 +1,4 @@
 export const PLAYER_SPEED: number = 160;
-export const COIN_SCORE: number = 10;
 
 export const WORLD_WIDTH: number = 2560;
 export const WORLD_HEIGHT: number = 1280;
@@ -11,5 +10,4 @@ export const UI_COLORS = {
   HEALTH_BAR_BG: 0x888888,
   HEALTH_BAR_FILL: 0xff0000,
   HEALTH_BAR_LOW: 0xff6600,
-  SCORE_TEXT: 0x000000
 };
