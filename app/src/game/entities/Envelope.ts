612f16e06530ef4caaf00980561e215214ef3eba
Gabi Delcea <vioriiiee@gmail.com>
2025-06-09 11:41:45 +0300

    unused player parameters

--- app/src/game/entities/Envelope.ts
+++ app/src/game/entities/Envelope.ts
@@ -38,7 +38,7 @@ export class Envelope extends Item {
     }
   }
 
-  onInteract(player: Phaser.Physics.Arcade.Sprite): void {
+  onInteract(_player: Phaser.Physics.Arcade.Sprite): void {
     console.log("Envelope interacted with!");
   }
 
