f3b7abf0412dd24f2b6b0dedc6c64ce56f44cdee
Lilach E <lilachelayza@gmail.com>
2025-08-13 18:03:03 +0300

    update GameState

--- app/src/stores/GameState.ts
+++ app/src/stores/GameState.ts
@@ -1,3 +1,10 @@
 export const GameState = {
   currentLevel: 1,
+  completedLevels: [] as number[],
+
+  markLevelCompleted(level: number) {
+    if (!this.completedLevels.includes(level)) {
+      this.completedLevels.push(level);
+    }
+  },
 };
