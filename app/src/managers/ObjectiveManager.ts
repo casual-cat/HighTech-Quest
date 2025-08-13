d5e75b84184ead39a8fb49897eedbaad657dce1d
Lilach E <lilachelayza@gmail.com>
2025-08-13 15:53:15 +0300

    add Objective Manager

--- app/src/managers/ObjectiveManager.ts
+++ app/src/managers/ObjectiveManager.ts
@@ -1,26 +1,44 @@
-interface SubObjective {
-  label: string;
-  completed: boolean;
-}
+import { LEVELS_DATA } from "../game/data/levelData";
 
-interface Objective {
-  title: string;
-  subtasks?: SubObjective[];
-  completed: boolean;
-}
+export const ObjectiveManager = {
+  completeTask(levelNumber: number, id: string) {
+    const level = LEVELS_DATA[levelNumber];
+    if (!level) return;
 
-class ObjectiveManager {
-  static currentObjective: Objective = {
-    title: "Find all CV pieces in the room",
-    completed: false,
-    subtasks: [],
-  };
+    for (const objective of level.objectives) {
+      if (objective.id === id) {
+        objective.complete = true;
+        return;
+      }
 
-  static setObjective(objective: Objective) {
-    this.currentObjective = objective;
-  }
+      if (objective.subtasks) {
+        for (const subtask of objective.subtasks) {
+          if (subtask.id === id) {
+            subtask.complete = true;
 
-  static getObjective(): Objective {
-    return this.currentObjective;
-  }
-}
+            if (objective.subtasks.every((st) => st.complete)) {
+              objective.complete = true;
+            }
+
+            return;
+          }
+        }
+      }
+    }
+  },
+
+  resetTasks(levelNumber: number) {
+    const level = LEVELS_DATA[levelNumber];
+    if (!level) return;
+
+    for (const objective of level.objectives) {
+      objective.complete = false;
+
+      if (objective.subtasks) {
+        for (const subtask of objective.subtasks) {
+          subtask.complete = false;
+        }
+      }
+    }
+  },
+};
