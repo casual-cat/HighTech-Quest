a1b3c3fb314986bb9bf6bc1ca040123e6f53ae98
Lilach E <lilachelayza@gmail.com>
2025-09-17 12:35:52 +0300

    add pathfinding to ben

--- app/src/game/entities/Ben.ts
+++ app/src/game/entities/Ben.ts
@@ -1,7 +1,12 @@
 import Phaser from "phaser";
 import { Character } from "./Character";
+import { WORLD } from "../constants/game";
+import { moveCharacterToTile } from "../utils/pathfinding";
 
 export class Ben extends Character {
+  private player?: Phaser.Physics.Arcade.Sprite;
+  private hasPathfoundToPlayer = false;
+
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 1, "ben");
 
@@ -10,28 +15,45 @@ export class Ben extends Character {
     scene.physics.add.existing(this, true);
 
     this.disableMovement();
-    this.createAnimations();
-    this.anims.play("ben-idle", true);
-
     this.setImmovable(true);
   }
 
-  public override createAnimations() {
-    const animKey = "ben-idle";
-    if (this.scene.anims.exists(animKey)) return;
+  public setPlayer(player: Phaser.Physics.Arcade.Sprite) {
+    this.player = player;
+    this.startPathfindingToPlayer();
+  }
 
-    this.scene.anims.create({
-      key: animKey,
-      frames: this.scene.anims.generateFrameNumbers("ben", {
-        start: 2,
-        end: 2,
-      }),
-      frameRate: 4,
-      repeat: -1,
+  private startPathfindingToPlayer() {
+    if (!this.player || this.hasPathfoundToPlayer) return;
+
+    const playerTileX = Math.floor(this.player.x / WORLD.TILE.WIDTH);
+    const playerTileY = Math.floor(this.player.y / WORLD.TILE.HEIGHT);
+
+    console.log(
+      "Ben is pathfinding to player at tile:",
+      playerTileX + 1,
+      playerTileY
+    );
+
+    moveCharacterToTile(this, playerTileX + 1, playerTileY, () => {
+      console.log("Ben has reached the player");
+      this.hasPathfoundToPlayer = true;
+      if (this.player && "enableMovement" in this.player) {
+        (this.player as any).enableMovement();
+      }
     });
   }
 
   public interact(): void {
-    console.log("Ben interaction triggered!");
+    if (!this.player) {
+      console.log("Ben interaction triggered, but no player reference");
+      return;
+    }
+
+    if (this.hasPathfoundToPlayer) {
+      console.log("Ben interaction triggered");
+      return;
+    }
+    this.startPathfindingToPlayer();
   }
 }
