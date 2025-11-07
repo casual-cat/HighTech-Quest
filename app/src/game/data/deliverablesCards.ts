42f548aaf8e99f940b33fd57915092f5e02c0ce3
Lilach E <lilachelayza@gmail.com>
2025-11-07 10:45:14 +0200

    Fix DELIVERABLES_CARDS data

--- app/src/game/data/deliverablesCards.ts
+++ app/src/game/data/deliverablesCards.ts
@@ -8,7 +8,7 @@ export const DELIVERABLES_CARDS: DeliverableCard[] = [
   {
     id: 1,
     image: "card1",
-    pairId: 1,
+    pairId: 2,
   },
   {
     id: 2,
@@ -18,22 +18,22 @@ export const DELIVERABLES_CARDS: DeliverableCard[] = [
   {
     id: 3,
     image: "card3",
-    pairId: 2,
+    pairId: 4,
   },
   {
     id: 4,
     image: "card4",
-    pairId: 2,
+    pairId: 3,
   },
   {
     id: 5,
     image: "card5",
-    pairId: 3,
+    pairId: 6,
   },
   {
     id: 6,
     image: "card6",
-    pairId: 3,
+    pairId: 5,
   },
   {
     id: 7,
