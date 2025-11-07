8cac4ff09238a9e7bea6432dc2820d985b215dcc
Lilach E <lilachelayza@gmail.com>
2025-11-07 10:40:05 +0200

    add DELIVERABLES_CARDS data

---
+++ app/src/game/data/deliverablesCards.ts
@@ -0,0 +1,48 @@
+type DeliverableCard = {
+  id: number;
+  image: string;
+  pairId: number;
+};
+
+export const DELIVERABLES_CARDS: DeliverableCard[] = [
+  {
+    id: 1,
+    image: "card1",
+    pairId: 1,
+  },
+  {
+    id: 2,
+    image: "card2",
+    pairId: 1,
+  },
+  {
+    id: 3,
+    image: "card3",
+    pairId: 2,
+  },
+  {
+    id: 4,
+    image: "card4",
+    pairId: 2,
+  },
+  {
+    id: 5,
+    image: "card5",
+    pairId: 3,
+  },
+  {
+    id: 6,
+    image: "card6",
+    pairId: 3,
+  },
+  {
+    id: 7,
+    image: "card7",
+    pairId: 8,
+  },
+  {
+    id: 8,
+    image: "card8",
+    pairId: 7,
+  },
+];
